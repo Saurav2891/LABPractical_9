c9fddbec9cdd1f42ba14ff93eaf8c6c1caee801d
Saurav Ranjan <ranjansaurav21@gmail.com>
2022-01-26 23:18:25 +0530

    Lab_Practical_9

---
+++ app/src/main/java/com/saurav_ranjan/labexp_9/MainActivityTest.java
@@ -0,0 +1,5 @@
+package com.saurav_ranjan.labexp_9;
+
+public class MainActivityTest {
+
+}
---
+++ app/src/main/java/com/saurav_ranjan/labexp_9/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.saurav_ranjan.labexp_9;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
